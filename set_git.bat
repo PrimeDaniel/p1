069f42d75e7359e9b3edb958448d18415e0a50ac
PrimeDaniel <128545450+PrimeDaniel@users.noreply.github.com>
2024-01-20 18:02:08 +0200

    first commit

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,6 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/[set your github repository path].git
+git remote add origin https://github.com/PrimeDaniel/webProject1.git
 git push -u origin main
+
